9a97ec2b66ef16cf9bf57a73d67904853e225f78
Lucas Cazeto <lucas.cazeto@parcellab.com>
2022-06-09 00:37:05 +0200

    chore: added flatten logic to main file

--- main.ts
+++ main.ts
@@ -1,2 +1,19 @@
-const main = (arr: unknown) => {
+const isArray = (input: unknown): input is unknown[] => {
+  return Object.prototype.toString.call(input) === "[object Array]";
+};
+
+const flatten = (arr: unknown[]) => {
+  return arr.reduce((prev: unknown[], curr): unknown[] => {
+    if (isArray(curr)) {
+      return prev.concat(flatten(curr));
+    } else {
+      return prev.concat(curr);
+    }
+  }, [] as unknown[]);
+};
+
+export const main = (input: unknown) => {
+  if (!isArray(input)) throw new TypeError("Input is not an array");
+
+  return flatten(input);
 };

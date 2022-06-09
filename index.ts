df34a2b19f50e08d6a9f61512a8314ef0c04341b
Lucas Cazeto <lucas.cazeto@parcellab.com>
2022-06-09 14:14:58 +0200

    chore: adjust configs

--- index.ts
+++ index.ts
@@ -2,18 +2,22 @@ const isArray = (input: unknown): input is unknown[] => {
   return Object.prototype.toString.call(input) === "[object Array]";
 };
 
-const flatten = (arr: unknown[]) => {
+const flattener = (arr: unknown[]) => {
   return arr.reduce((prev: unknown[], curr): unknown[] => {
     if (isArray(curr)) {
-      return prev.concat(flatten(curr));
+      return prev.concat(flattener(curr));
     } else {
       return prev.concat(curr);
     }
   }, [] as unknown[]);
 };
 
-export const main = (input: unknown) => {
+const main = (input: unknown) => {
   if (!isArray(input)) throw new TypeError("Input is not an array");
 
-  return flatten(input);
+  return flattener(input);
 };
+
+console.log(main([1, 2, [3, 4]]));
+
+export default main;
